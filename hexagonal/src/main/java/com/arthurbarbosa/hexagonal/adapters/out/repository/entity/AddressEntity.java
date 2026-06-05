304229e3a7330d25d151d8e21ec07548222f6033
Arthur Barbosa <arthurbruno03@gmail.com>
2026-06-05 17:50:02 -0300

    feat(hexagonal): create class AddressEntity, CustomerEntity and CustomerRepository

---
+++ hexagonal/src/main/java/com/arthurbarbosa/hexagonal/adapters/out/repository/entity/AddressEntity.java
@@ -0,0 +1,10 @@
+package com.arthurbarbosa.hexagonal.adapters.out.repository.entity;
+
+import lombok.Data;
+
+@Data
+public class AddressEntity {
+    private String city;
+    private String state;
+    private String street;
+}
